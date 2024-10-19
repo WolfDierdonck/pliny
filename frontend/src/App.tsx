7eda34b9495c488ef00404c5c76977ef95ddd2bf
Tara Garg <tara.garg24@gmail.com>
2024-10-19 19:53:53 -0400

    initialized a react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import BubbleChart from "./components/BubbleChart";
+import Timeline from "./components/Timeline";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bubble-chart" element={<BubbleChart />} />
+        <Route path="/timeline" element={<Timeline />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Timeline.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Timeline: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Timeline Visualization</h1>
+      <p className="mt-4">This is where the Timeline will be displayed.</p>
+    </div>
+  );
+};
+
+export default Timeline;
