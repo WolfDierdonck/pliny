118e72d2d4855242a820de09a00e95f48237e1a7
Wolf Van Dierdonck <wolf.vandierdonck@gmail.com>
2024-10-19 21:25:17 -0400

    action should work

--- data-pipeline/ingestion/analytics_api/analytics_api.py
+++ data-pipeline/ingestion/analytics_api/analytics_api.py
@@ -8,7 +8,7 @@ import datetime
 
 
 class AnalyticsAPI:
-    def __init__(self):
+    def __init__(self) -> None:
         self.project = "en.wikipedia.org"
         self.url = "https://wikimedia.org/api/rest_v1/metrics"
         self.headers = {
