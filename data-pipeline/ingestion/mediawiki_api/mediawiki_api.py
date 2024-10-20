87e242af0fa185dc26ce741affcbbd1d6394a525
Wolf Van Dierdonck <wolf.vandierdonck@gmail.com>
2024-10-19 21:16:18 -0400

    Move everything over

--- data-pipeline/ingestion/mediawiki_api/mediawiki_api.py
+++ data-pipeline/ingestion/mediawiki_api/mediawiki_api.py
@@ -2,6 +2,7 @@ import concurrent.futures
 from concurrent.futures import Future
 import requests
 
+
 class MediaWikiAPI:
     def __init__(self) -> None:
         self.url = "https://en.wikipedia.org/w/api.php"
@@ -10,10 +11,12 @@ class MediaWikiAPI:
             "From": "wolf.vandierdonck@gmail.com",
         }
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=100)
-    
-    def get_all_pages_starting_from(self, page_name: str) -> Future[tuple[list[str], str | None]]:
+
+    def get_all_pages_starting_from(
+        self, page_name: str
+    ) -> Future[tuple[list[str], str | None]]:
         def fetch_and_process(page_name: str) -> tuple[list[str], str | None]:
-            # filter out redirects as many symbols redirect to pages we've already seen; ⬢ redirects to Hexagon 
+            # filter out redirects as many symbols redirect to pages we've already seen; ⬢ redirects to Hexagon
             # redirects also result in 404 when querying for edit count on wikimedia analytics api
             params = {
                 "action": "query",
@@ -31,11 +34,11 @@ class MediaWikiAPI:
 
             pages = data["query"]["allpages"]
             page_names: list[str] = [page["title"] for page in pages]
-            
+
             page_to_continue_from = None
             if "continue" in data and "apcontinue" in data["continue"]:
                 page_to_continue_from = data["continue"]["apcontinue"]
 
             return (page_names, page_to_continue_from)
-            
+
         return self.executor.submit(fetch_and_process, page_name)
