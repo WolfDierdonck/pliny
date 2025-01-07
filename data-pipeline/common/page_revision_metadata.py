9ce62f81bc0425f6e0238a97feed74cc5d1e6511
Jimmy Huynh <huynhj2002@gmail.com>
2025-01-07 15:28:57 -0500

    oops missed an assignment

--- data-pipeline/common/page_revision_metadata.py
+++ data-pipeline/common/page_revision_metadata.py
@@ -11,6 +11,7 @@ class PageRevisionMetadata:
         self.editor_count = editor_count
         self.revert_count = revert_count
         self.net_bytes_change = net_bytes_change
+        self.revert_bytes_change = revert_bytes_change
 
     def __str__(self) -> str:
         return f"revision_count={self.revision_count}, editor_count={self.editor_count}, revert_count={self.revert_count}, net_bytes_change={self.net_bytes_change}"
