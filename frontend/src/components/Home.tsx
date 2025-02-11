cb02f768b375849037d3d510be0801835380d460
Matt Zhang <matt@zhang.software>
2025-02-11 18:21:00 -0500

    first graph

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,52 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import {
-  getTopVandalismData,
-  getTopViewsData,
-  TopVandalismData,
-  TopViewsData,
-} from '../lib/api';
+import React from 'react';
+import TopViewsBarGraph from './visualizations/TopViewsBarGraph';
 
 const Home = () => {
-  const [viewData, setViewData] = useState<TopViewsData[]>([]);
-  const [vandalismData, setVandalismData] = useState<TopVandalismData[]>([]);
-
-  useEffect(() => {
-    getTopViewsData('2024-09-01', 10)
-      .then((data) => {
-        setViewData(data);
-      })
-      .catch((error) => {
-        console.error('Failed to get data', error);
-      });
-  }, []);
-
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">
-        Welcome to the Data Visualization Website
-      </h1>
-      <p className="mt-4">
-        This website provides different visualizations based on the data queried
-        from our backend.
-      </p>
-
-      <p className="mt-4 font-bold">The view data from BigQuery is:</p>
-      <ul>
-        {viewData.map((item: TopViewsData) => (
-          <li key={item.page_name}>
-            {item.page_name}: {item.view_count}
-          </li>
-        ))}
-      </ul>
-
-      <p className="mt-4 font-bold">The vandalism data from BigQuery is:</p>
-      <ul>
-        {vandalismData.map((item: TopVandalismData) => (
-          <li key={item.page_name}>
-            {item.page_name}: {item.percent_reverted}
-          </li>
-        ))}
-      </ul>
+    <div className="newspaper-layout">
+      <header className="header newspaper-header">
+        <h1 className="title newspaper-title">Pliny</h1>
+        <p className="subtitle newspaper-subtitle">
+          Wikipedia is largest and most comprehensive source of information in
+          the world. Unlike the encyclopedias of the past, Wikipedia is a living
+          document that is constantly updated by volunteers from around the
+          world. This project explores the real-time patterns and trends in this
+          knowledge.
+        </p>
+      </header>
+      <main className="content">
+        <section className="main-article">
+          <h2 className="section-title">Top Views</h2>
+          <TopViewsBarGraph />
+        </section>
+        <aside className="sidebar">
+          <h2 className="section-title">Most Edited Pages</h2>
+          <p>
+            Knowledge is constantly changing on Wikipedia. This month, the most
+            edited pages are about XXXX, XXXX, and XXXX.
+          </p>
+        </aside>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2024 Pliny. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
