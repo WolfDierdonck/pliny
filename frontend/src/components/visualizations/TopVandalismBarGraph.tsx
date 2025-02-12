b9e60797a710c1dc0c7034060c9cad9da89a010e
Matt Zhang <matt@zhang.software>
2025-02-11 19:22:41 -0500

    top growing

---
+++ frontend/src/components/visualizations/TopVandalismBarGraph.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+  Legend,
+} from 'recharts';
+import { getTopVandalismData, TopVandalismData } from '../../lib/api';
+
+type TopVandalizedWithPercentKept = TopVandalismData & { percent_kept: number };
+
+const TopVandalismBarGraph = () => {
+  const [vandalizedData, setVandalizedData] = useState<
+    TopVandalizedWithPercentKept[]
+  >([]);
+
+  useEffect(() => {
+    getTopVandalismData('2024-09-01', '2024-09-07', 10)
+      .then((data) => {
+        const vandalizedDataWithPercentKept = data.map((data) => ({
+          ...data,
+          page_name: data.page_name.replace(/_/g, ' '),
+          percent_reverted: (data.revert_count / data.edit_count) * 100,
+          percent_kept: 100 - (data.revert_count / data.edit_count) * 100,
+        }));
+        setVandalizedData(vandalizedDataWithPercentKept);
+      })
+      .catch((error) => console.error('Failed to get data', error));
+  }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height={500} style={{ padding: 20 }}>
+      <BarChart data={vandalizedData}>
+        <CartesianGrid />
+        <XAxis dataKey="page_name" angle={-45} textAnchor="end" height={100} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {/* <Bar dataKey="view_count" stackId="a" fill="#8884d8" /> */}
+        {/* <Bar dataKey="revert_count" stackId="a" fill="#82ca9d" /> */}
+        {/* <Bar dataKey="bytes_reverted" stackId="a" fill="#ffc658" /> */}
+        <Bar dataKey="percent_reverted" stackId="a" fill="#ff82ab" />
+        <Bar dataKey="percent_kept" stackId="a" fill="#ffc658" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default TopVandalismBarGraph;
