f9d0285fde56aec92bda2210a118e38ccf9e4286
Matt Zhang <matt@zhang.software>
2025-02-12 22:50:31 -0500

    all the other trends

--- frontend/src/components/visualizations/TopVandalismBarGraph.tsx
+++ frontend/src/components/visualizations/TopVandalismBarGraph.tsx
@@ -19,14 +19,17 @@ const TopVandalismBarGraph = () => {
   >([]);
 
   useEffect(() => {
-    getTopVandalismData('2024-09-01', '2024-09-07', 10)
+    getTopVandalismData('2024-09-07', 10)
       .then((data) => {
-        const vandalizedDataWithPercentKept = data.map((data) => ({
-          ...data,
-          page_name: data.page_name.replace(/_/g, ' '),
-          percent_reverted: (data.revert_count / data.edit_count) * 100,
-          percent_kept: 100 - (data.revert_count / data.edit_count) * 100,
-        }));
+        const vandalizedDataWithPercentKept = data
+          // .sort(() => 0.5 - Math.random())
+          // .slice(0, 10)
+          .map((data) => ({
+            ...data,
+            page_name: data.page_name.replace(/_/g, ' '),
+            percent_reverted: (data.revert_count / data.edit_count) * 100,
+            percent_kept: 100 - (data.revert_count / data.edit_count) * 100,
+          }));
         setVandalizedData(vandalizedDataWithPercentKept);
       })
       .catch((error) => console.error('Failed to get data', error));
@@ -41,10 +44,10 @@ const TopVandalismBarGraph = () => {
         <Tooltip />
         <Legend />
         {/* <Bar dataKey="view_count" stackId="a" fill="#8884d8" /> */}
-        {/* <Bar dataKey="revert_count" stackId="a" fill="#82ca9d" /> */}
+        <Bar dataKey="revert_count" stackId="a" fill="#82ca9d" />
         {/* <Bar dataKey="bytes_reverted" stackId="a" fill="#ffc658" /> */}
-        <Bar dataKey="percent_reverted" stackId="a" fill="#ff82ab" />
-        <Bar dataKey="percent_kept" stackId="a" fill="#ffc658" />
+        {/* <Bar dataKey="percent_reverted" stackId="a" fill="#ff82ab" /> */}
+        {/* <Bar dataKey="percent_kept" stackId="a" fill="#ffc658" /> */}
       </BarChart>
     </ResponsiveContainer>
   );
