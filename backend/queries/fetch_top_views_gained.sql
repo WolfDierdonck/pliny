12663d77267dbb060c3b88e96634327d8d31498d
Jimmy Huynh <huynhj2002@gmail.com>
2025-03-01 18:32:39 -0500

    update top views gains/lost

--- backend/queries/fetch_top_views_gained.sql
+++ backend/queries/fetch_top_views_gained.sql
@@ -2,7 +2,8 @@ SELECT
   date as date,
   page_name,
   current_view_count,
-  previous_view_count,
+  one_day_ago_view_count,
+  two_days_ago_view_count
   view_count_ratio
 FROM wikipedia_data.top_views_gained_final_table
 WHERE date = {{date}}
